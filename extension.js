2b6d9b5a9cb34b697e640685b88d1ad5f2430f52
Joshua Huber <joshua@mrhuber.com>
2022-04-23 21:27:42 -0400

    Removed dependence on Lang.

--- extension.js
+++ extension.js
@@ -4,7 +4,6 @@ const Main = imports.ui.main;
 const Mainloop = imports.mainloop;
 
 const St = imports.gi.St;
-const Lang = imports.lang;
 const PanelMenu = imports.ui.panelMenu;
 const Clutter = imports.gi.Clutter;
 const GLib = imports.gi.GLib;
@@ -62,7 +61,7 @@ var LanIPAddressIndicator = class LanIPAddressIndicator extends PanelMenu.Button
                 Mainloop.source_remove(this._timeout);
                 this._timeout = null;
         }
-        this._timeout = Mainloop.timeout_add_seconds(refreshTime, Lang.bind(this, this._updateLabel));
+        this._timeout = Mainloop.timeout_add_seconds(refreshTime, () => {this._updateLabel();});
 
         this.buttonText.set_text(_get_lan_ip());
     }
